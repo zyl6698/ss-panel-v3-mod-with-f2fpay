807600603f61420775d6a3c3437588076da2ad82
zyl6698 <zyl.6698@gmail.com>
2017-11-01 00:27:26 +0800

    change qr api

--- tool/alipay-f2fpay/f2fpay/service/AlipayTradeService.php
+++ tool/alipay-f2fpay/f2fpay/service/AlipayTradeService.php
@@ -371,9 +371,9 @@ class AlipayTradeService {
 		return $image;
 	}
 	*/
-	 function create_erweima($content, $size = '200', $lev = 'L', $margin= '0') {
+	   function create_erweima($content, $size = '200', $lev = 'L', $margin= '0') {
     $url = urlencode($content);
-    $image = '<a href="'.$content.'" target="_blank"><img src="https://tool.kd128.com/qrcode?text='.$content.'&w='.$size.'" />';
+    $image = '<a href="'.$content.'" target="_blank"><img src="https://pan.baidu.com/share/qrcode?w='.$size.'&amp;h='.$size.'&amp;url='.$url.'"  widht="'.$size.'" height="'.$size.'" />';
     return $image;
   }
 	function create_erweima64($content, $size = '200', $lev = 'L', $margin= '0') {
