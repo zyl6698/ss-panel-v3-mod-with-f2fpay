5800542e4ea56c44c6da26241b531a2dc8b4d753
zyl6698 <zyl.6698@gmail.com>
2018-08-27 23:06:21 +1000

    修复f2f二维码显示失败

--- tool/alipay-f2fpay/f2fpay/service/AlipayTradeService.php
+++ tool/alipay-f2fpay/f2fpay/service/AlipayTradeService.php
@@ -39,10 +39,10 @@ class AlipayTradeService {
 
 
 	public $token = NULL;
-	
+
 	//重试次数
 	private $MaxQueryRetry;
-	
+
 	//重试间隔
 	private $QueryDuration;
 
@@ -92,7 +92,7 @@ class AlipayTradeService {
 	function AlipayWapPayService($alipay_config) {
 		$this->__construct($alipay_config);
 	}
-	
+
 	// 当面付2.0条码支付(带轮询逻辑)
 	public function barPay($req) {
 
@@ -104,7 +104,7 @@ class AlipayTradeService {
 		$this->writeLog($bizContent);
 
 		//echo $bizContent;
-		
+
 		$request = new AlipayTradePayRequest();
 		$request->setBizContent ( $bizContent );
 
@@ -311,8 +311,8 @@ class AlipayTradeService {
 		return empty($response)||
 					$response->code == "20000";
 	}
-	
-	
+
+
 	public function cancel($cancelContentBuilder) {
 		$biz_content= $cancelContentBuilder->getBizContent();
 		$this->writeLog($biz_content);
@@ -359,23 +359,18 @@ class AlipayTradeService {
 		//file_put_contents ( "log/log.txt", date ( "Y-m-d H:i:s" ) . "  " . $text . "\r\n", FILE_APPEND );
 	}
 
-	/** *利用google api生成二维码图片
+	/* *利用google api生成二维码图片
 	 * $content：二维码内容参数
 	 * $size：生成二维码的尺寸，宽度和高度的值
 	 * $lev：可选参数，纠错等级
 	 * $margin：生成的二维码离边框的距离
-	 
+    */
 	function create_erweima($content, $size = '200', $lev = 'L', $margin= '0') {
 		$content = urlencode($content);
 		$image = '<img src="http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&amp;cht=qr&chld='.$lev.'|'.$margin.'&amp;chl='.$content.'"  widht="'.$size.'" height="'.$size.'" />';
 		return $image;
 	}
-	*/
-	   function create_erweima($content, $size = '200', $lev = 'L', $margin= '0') {
-    $url = urlencode($content);
-    $image = '<a href="'.$content.'" target="_blank"><img src="https://pan.baidu.com/share/qrcode?w='.$size.'&amp;h='.$size.'&amp;url='.$url.'"  widht="'.$size.'" height="'.$size.'" />';
-    return $image;
-  }
+	
 	function create_erweima64($content, $size = '200', $lev = 'L', $margin= '0') {
 		$content = urlencode($content);
 		$imageurl = 'http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&cht=qr&chld='.$lev.'|'.$margin.'&chl='.$content;
